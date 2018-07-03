0ed9eac1dd520e713c99fc4cc89f6f320fd51518
pdmxdd <pdmxdd@gmail.com>
2018-07-03 15:21:27 -0500

    Formatting

--- cld.py
+++ cld.py
@@ -8,10 +8,10 @@ def print_separator():
     print("-" * 40)
 
 if __name__ == "__main__":
-    for i in range(3):
+    #for i in range(3):
         clear_screen()
         print_separator()
         addr = "511 Joshua Drive, Crestwood, MO"
         get_weather_from_address(addr)
         print_separator()
-        time.sleep(120)
+        #time.sleep(120)
